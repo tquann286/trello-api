f652c4dc5f9f3fea6df64321d378c8cbac8eafee
Quan Trung <quantrung286@gmail.com>
2022-04-20 14:55:20 +0700

    up

--- src/server.js
+++ src/server.js
@@ -2,14 +2,23 @@ import express from 'express'
 import { connectDB } from '*/config/mongodb'
 import { env } from '*/config/environment'
 
-const app = express()
+connectDB()
+  .then(() => console.log('Connected successfully to database server!'))
+  .then(() => bootServer())
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
 
-connectDB().catch(console.log)
+const bootServer = () => {
+  const app = express()
 
-app.get('/', (req, res) => {
-  res.end('<h1>Hello World!</h1><hr/>')
-})
-
-app.listen(env.APP_PORT, env.APP_HOST, () => {
-  console.log(`Hello, I'm running at ${env.APP_HOST}:${env.APP_PORT}/`)
-})
+  app.get('/', async (req, res) => {
+    
+    res.end('<h1>Hello World!</h1><hr/>')
+  })
+  
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello, I'm running at ${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+}
