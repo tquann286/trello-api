7db5ac7a6eae3b850b3c310130d78a6b62980d93
Quan Trung <quantrung286@gmail.com>
2022-04-20 21:24:53 +0700

    up

--- src/server.js
+++ src/server.js
@@ -13,10 +13,6 @@ connectDB()
 
 const bootServer = () => {
   const app = express()
-
-  app.get('/test', async (req, res) => {
-    res.end('<h1>Hello World!</h1><hr/>')
-  })
   
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Hello, I'm running at ${env.APP_HOST}:${env.APP_PORT}/`)
